cb44e8f558bf055c3c115f39406e03294f0cd0ca
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2021-02-05 15:22:17 -0700

    Adjust break parameters for input, also parameterize the search string with hyphens

--- bin/main.rb
+++ bin/main.rb
@@ -8,14 +8,14 @@ puts 'By: Garrett Gerdsen'
 puts ''
 puts '*********************************************************************'
 puts ''
-puts 'Which website do you want to scrape? (indeed only at this time...)'
+puts 'Which website do you want to scrape? (Indeed only at this time...)'
 
 # user input for job search engine here
 input = ''
 loop do
   input = gets.chomp
-  break if ['udacity', 'indeed', 'remote.io'].include?(input.downcase)
-  puts 'Error, please enter a valid selection (indeed only at this time...)'
+  break if ['indeed'].include?(input.downcase)
+  puts 'Error, please enter a valid selection (Indeed only at this time...)'
 end
 
 puts 'Enter a job title or other search string'
@@ -23,10 +23,12 @@ puts 'Enter a job title or other search string'
 search = ""
 search = gets.chomp
 
+# this adds a hyphen if there is whitespace between search terms
+paramerized_search = search.gsub(/[\s\-]+/, '-')
 
 # calls the IndeedScraper class
 if input == "indeed"
-  url = "https://www.indeed.com/jobs?q=#{search}&l=Remote&explvl=entry_level&sort=date"
+  url = "https://www.indeed.com/jobs?q=#{paramerized_search}&l=Remote&explvl=entry_level&sort=date"
   website = IndeedScraper.new(url)
 end
 
