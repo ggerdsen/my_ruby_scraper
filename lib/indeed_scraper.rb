118e15ee53da4a1015752831b9a13f8d50ee989e
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2021-02-05 14:49:06 -0700

    Add logic that appends link parameters dependant upon the path for `url`

--- lib/indeed_scraper.rb
+++ lib/indeed_scraper.rb
@@ -12,13 +12,13 @@ class IndeedScraper < Scraper
   end
 
   def scrape
-    parsed_page = parsing_page(@url)
+    parsed_page = parsing_page(@url)  # calls private method `parsing_page` within the Scraper parent class
     total_pages = total_pages_finder(parsed_page, 'div.searchCount-a11y-contrast-color')
     pages_append_urls = page_ending_urls(total_pages)
     scrape_per_page(pages_append_urls)
+    sorted_arr = sort_by_dates(@result)
+    write('indeed_jobs.csv', sorted_arr, 'jobs')
     
-    
-    binding.pry
   end
   
   def total_pages_finder(parsed_page, page_css_property)
@@ -33,13 +33,38 @@ class IndeedScraper < Scraper
     urls_arr.each do |page|
       each_page_url = @url + "&start=#{page}"
       parsed_page = parsing_page(each_page_url)
-      jobs_listings = parsed_page.css('div.jobsearch-SerpJobCard')
-      add_jobs(jobs_listings)
-      puts "#{@result.length - 1} Ruby-on-Rails jobs have been scraped from indeed.com..."
+      job_listings = parsed_page.css('div.jobsearch-SerpJobCard')
+      add_jobs(job_listings)
+      puts "#{@result.length - 1} Ruby on Rails jobs have been scraped from indeed.com..."
     end
   end
   
   def sort_by_dates(arr)
     [arr[0]] + arr[1..-1].sort_by { |str| str.split(',')[-1][0, 2].to_i }
+  end
+  
+  def add_jobs(job_listings)
+    job_listings.each do |listing|
+      title = listing.css('a.jobtitle').text.gsub("\n", '').gsub(',', ' ')
+      company = listing.css('span.company').text.gsub("\n", '').gsub(',', ' ')
+      location = 'remote' # css is 'span.location' if needed
+      summary = listing.css('div.summary').text.gsub("\n", '').gsub(',', ' ')
+      key_url = listing.css('a')[0].attributes['href'].value[7..-1]
+      url = append_link_type(key_url)
+      # url = key_url.start_with?('?jk=') ? 'https://www.indeed.com/viewjob' + key_url : ''
+      day_posted = listing.css('span.date').text.gsub("\n", '')
+      @result << "#{title},#{company},#{location},#{summary},#{url},#{day_posted}"
+    end
+  end
+  
+  # appends link parameters dependant upon the path
+  def append_link_type(key_url)
+    if key_url.start_with?('y/')
+      'https://www.indeed.com/compan' + key_url
+    elsif key_url.start_with?('?jk=')
+      'https://www.indeed.com/viewjob' + key_url
+    elseif key_url.start_with?('/clk?mo')
+      'https://www.indeed.com/pagead' + key_url
+    end
   end
 end
